5ea69b3951352c9ca3f00355f306ab143af36d4e
ifer-itcast <yangkangkang@itcast.cn>
2019-11-21 14:37:03 +0800

    新增用户功能

--- route/admin.js
+++ route/admin.js
@@ -9,4 +9,10 @@ admin.get('/logout', require('./admin/logout'));
 
 admin.post('/login', require('./admin/login'));
 
+// 新增用户界面
+admin.get('/user-edit', require('./admin/user-edit'));
+
+// 新增用户功能
+admin.post('/user-edit', require('./admin/user-edit-fn'));
+
 module.exports = admin;
