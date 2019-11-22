0d3c7ea4b5c444d9c926a2c6d58517a98ed70f4b
ifer-itcast <yangkangkang@itcast.cn>
2019-11-22 10:44:53 +0800

    区分是添加还是修改

--- route/admin/user-edit.js
+++ route/admin/user-edit.js
@@ -1,6 +1,20 @@
-module.exports = (req, res) => {
-    const {message} = req.query;
-    res.render('admin/user-edit', {
-        message
-    });
+const {User} = require('../../model/user');
+
+module.exports = async (req, res) => {
+    const {message, id} = req.query;
+    if(id) {
+        // 修改
+        const user = await User.findOne({_id: id})
+        res.render('admin/user-edit', {
+            message,
+            user,
+            link: '/admin/user-add'
+        });
+    } else {
+        // 添加
+        res.render('admin/user-edit', {
+            message,
+            link: '/admin/user-edit'
+        });
+    }
 };
