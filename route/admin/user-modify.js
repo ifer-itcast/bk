43080769e85d3b5ccf2574b6676d9bc331144132
ifer-itcast <yangkangkang@itcast.cn>
2019-11-22 11:49:51 +0800

    用户信息修改功能

--- route/admin/user-modify.js
+++ route/admin/user-modify.js
@@ -1,15 +1,36 @@
-const {User}  = require('../../model/user');
+const {
+    User
+} = require('../../model/user');
 const bcrypt = require('bcrypt');
 module.exports = async (req, res, next) => {
-    const { id } = req.query;
-    const postUser = req.body;
+    const {
+        id
+    } = req.query;
+    const {
+        username,
+        email,
+        role,
+        state,
+        password
+    } = req.body;
 
-    let findUser = await User.findOne({_id: id});
+    let findUser = await User.findOne({
+        _id: id
+    });
 
-    let isValid = await bcrypt.compare(postUser.password, findUser.password)
-    if(isValid) {
+    let isValid = await bcrypt.compare(password, findUser.password)
+    if (isValid) {
         // 允许修改
-        res.send('ok');
+        // res.send('ok');
+        await User.updateOne({
+            _id: id
+        }, {
+            username,
+            email,
+            role,
+            state
+        });
+        res.redirect('/admin/user');
     } else {
         // 密码传入错误，不允许修改
         let obj = {
