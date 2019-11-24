9aa31f52f6e438822069d6c3b8cda7d1a56c138b
ifer-itcast <yangkangkang@itcast.cn>
2019-11-24 16:41:51 +0800

    .

--- route/home/index.js
+++ route/home/index.js
@@ -1,4 +1,11 @@
-module.exports = (req, res) => {
-    // res.send('首页');
-    res.render('home/default.art')
+const {Article} = require('../../model/article');
+const pagination = require('mongoose-sex-page');
+
+module.exports = async (req, res) => {
+    let page = req.query.page || 1;
+    let articles = await pagination(Article).find().populate('author').page(page).size(3).display(5).exec();
+    // res.send(articles);
+    res.render('home/default', {
+        articles
+    });
 };
