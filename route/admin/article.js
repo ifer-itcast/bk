ed63c29aa4a7cf4dfd0a13965a6bdd1b0196b552
ifer-itcast <yangkangkang@itcast.cn>
2019-11-24 16:02:17 +0800

    补上文章分页

--- route/admin/article.js
+++ route/admin/article.js
@@ -1,7 +1,9 @@
 const {Article} = require('../../model/article');
+const paganation = require('mongoose-sex-page');
 module.exports = async (req,res, next) => {
     req.app.locals.currentLink = 'article';
-    const articles = await Article.find().populate('author');
+    let page = req.query.page || 1;
+    const articles = await paganation(Article).find().page(page).size(2).display(5).populate('author').exec();
     res.render('admin/article', {
         articles
     });
