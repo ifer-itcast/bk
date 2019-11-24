ff65ca25baff4f57e8eee3dd9683a8230f05c18e
ifer-itcast <yangkangkang@itcast.cn>
2019-11-24 14:53:26 +0800

    config

--- model/connect.js
+++ model/connect.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
+const config = require('config');
 mongoose.set('useCreateIndex', true);
-mongoose.connect('mongodb://ifer:ifer@localhost/blog6', {
+
+// mongodb://itcast:itcast@localhost/blog2
+// mongodb://localhost/blog2
+// mongodb://${config.get('db.user')}:${config.get('db.pwd')}@${config.get('db.host')}:${config.get('db.port')}/${config.get('db.name')}
+// console.log(config.get('db.pwd'))
+mongoose.connect(`mongodb://${config.get('db.user')}:${config.get('db.pwd')}@${config.get('db.host')}:${config.get('db.port')}/${config.get('db.name')}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(() => console.log('数据库连接成功')).catch(err => {
-    console.log(err, '数据库连接失败');
-});
+}).then(() => console.log('数据库连接成功')).catch((err) => console.log(err,'数据库连接失败'));
+
