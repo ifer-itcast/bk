b4cfb0a011bd2dd2fd7c2ccd80b0156896e91fdf
ifer-itcast <yangkangkang@itcast.cn>
2019-11-24 16:49:05 +0800

    mongoose-sex-page

--- route/admin/article.js
+++ route/admin/article.js
@@ -1,7 +1,11 @@
-const {Article} = require('../../model/article');
+const paganition = require('mongoose-sex-page');
+
 module.exports = async (req,res, next) => {
+    let page = req.query.page || 1;
     req.app.locals.currentLink = 'article';
-    const articles = await Article.find().populate('author');
+    const articles = await paganition(Article).find().page(page).size(2).display(4).populate('author').exec();
+    // console.log(articles.display);
+    // res.send(articles);
     res.render('admin/article', {
         articles
     });
