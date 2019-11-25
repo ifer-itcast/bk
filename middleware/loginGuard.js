8fb1289069f5380db6d5f397c1a151e99f436ec3
ifer-itcast <yangkangkang@itcast.cn>
2019-11-25 16:58:20 +0800

    .

--- middleware/loginGuard.js
+++ middleware/loginGuard.js
@@ -2,10 +2,15 @@ module.exports = (req, res, next) => {
     if(req.url !== '/login' && !req.session.username) {
         res.redirect('/admin/login');
     } else {
+        console.log(req.url, req.session.role);
         // 已经登录的普通用户也应允许访问登录页面
-        if(req.url !== '/login' && req.session.role === 'normal') {
+        if(req.url !== '/login' && req.url !== '/logout' && req.session.role === 'normal') {
             return res.redirect('/home');
         }
         next();
     }
+
+    // 是否登陆页面
+    // 是否是管理员
+    // 拦截中间件放在哪里
 };
