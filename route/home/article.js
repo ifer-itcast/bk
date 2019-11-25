1fe8bcd7eaa92571b4937b748d581ac1e0bd923b
ifer-itcast <yangkangkang@itcast.cn>
2019-11-25 09:39:24 +0800

    文章详情

--- route/home/article.js
+++ route/home/article.js
@@ -1,3 +1,10 @@
-module.exports = (req, res) => {
-    res.render('home/article');
+const {Article} = require('../../model/article');
+
+module.exports = async (req, res) => {
+    const articles = await Article.findOne({
+        _id: req.query.id
+    }).populate('author');
+    res.render('home/article', {
+        articles
+    });
 };
