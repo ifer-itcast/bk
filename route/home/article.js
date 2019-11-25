ccc0efccc0cd9e6fcb6fba88135ec8020b5a3abe
ifer-itcast <yangkangkang@itcast.cn>
2019-11-25 11:01:57 +0800

    评论功能

--- route/home/article.js
+++ route/home/article.js
@@ -1,10 +1,17 @@
 const {Article} = require('../../model/article');
+const {Comment} = require('../../model/comment');
 
 module.exports = async (req, res) => {
     const articles = await Article.findOne({
         _id: req.query.id
     }).populate('author');
+
+    const comments = await Comment.find({
+        aid: req.query.id
+    }).populate('uid');
+    // res.send(comments);
     res.render('home/article', {
-        articles
+        articles,
+        comments
     });
 };
